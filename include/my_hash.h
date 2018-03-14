0e91ae28ca80fe974594eb96e0997127cee1a1b6
Maxmilian Toor <toorm@mail.gvsu.edu>
2018-03-14 16:12:25 -0400

    Created basic set up

---
+++ include/my_hash.h
@@ -0,0 +1,7 @@
+#define
+#ifndef
+#include <string>
+
+boolean insert(const Superhero & s);
+
+Superhero & get(const std::string name);
---
+++ include/superhero.h
@@ -0,0 +1,22 @@
+#define
+#ifndef
+#include <string>
+
+class superhero
+{
+	private:
+		int page_id;
+		std::string name;
+		std::string urlslug;
+		std::string id;
+		std::string alignment;
+		char eye_color;
+		char hair_color;
+		char sex;
+		std::string gsm;
+		boolean alive;
+		int appearances;
+		std::string first_appearance;
+		int year;
+
+};
