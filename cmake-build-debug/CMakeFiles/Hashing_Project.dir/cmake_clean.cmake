a14d78abf6c002367d0dd2240bff92587e308d42
Santiago Quiroga <quirogas@mail.gvsu.edu>
2018-03-21 03:01:11 -0400

    deleted system files

---
+++ cmake-build-debug/CMakeFiles/Hashing_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hashing_Project.dir/src/main.cpp.o"
+  "Hashing_Project.pdb"
+  "Hashing_Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hashing_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
